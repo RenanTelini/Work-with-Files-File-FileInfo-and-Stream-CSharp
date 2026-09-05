bc42fd2ff77515b217dddf3c4df983242a75590d
Renan Telini <renan_telini@hotmail.com>
2026-09-05 14:22:55 -0300

    File - Static members

---
+++ DemoFile/DemoFile/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DemoFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Windows\Temp\DemoFile - Using static members from File - C# course.txt";
+            string targetPath = @"C:\Windows\Temp\DemoFile - Using static members from File - C# course - 2.txt";
+
+            try
+            {
+                File.Copy(sourcePath, targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Occurred a error");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Work with Files - File, FileInfo and Stream in C#
+
+#### This exercise is based on the <a href="https://www.udemy.com/course/programacao-orientada-a-objetos-csharp/?couponCode=MT260714G2">"C# COMPLETO Programação Orientada a Objetos + Projetos"</a> course.
+
+### <ins>File - Static members</ins>
+
+- Static members (simple, but performs a security check for each operation)
