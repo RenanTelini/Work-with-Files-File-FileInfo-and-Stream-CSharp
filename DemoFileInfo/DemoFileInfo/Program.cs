f0a6428e4155857b1a72471b0ae5940af2817621
Renan Telini <renan_telini@hotmail.com>
2026-09-05 14:27:50 -0300

    Class FileInfo

---
+++ DemoFileInfo/DemoFileInfo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DemoFileInfo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sourcePath = @"C:\Windows\Temp\DemoFileInfo - Using functions from FileInfo class - C# course.txt";
+        string targetPath = @"C:\Windows\Temp\DemoFileInfo - Using functions from FileInfo class - C# course.txt - 2";
+
+        try
+        {
+            FileInfo fileInfo = new FileInfo(sourcePath);
+            fileInfo.CopyTo(targetPath);
+            string[] lines = File.ReadAllLines(sourcePath);
+            foreach(string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        catch(IOException e)
+        {
+            Console.WriteLine("Occurred a error");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 #### This exercise is based on the <a href="https://www.udemy.com/course/programacao-orientada-a-objetos-csharp/?couponCode=MT260714G2">"C# COMPLETO Programação Orientada a Objetos + Projetos"</a> course.
 
+#### This exercises covers the use of static members from File, FileInfo class and your functions, Using block, FileStream, StreamReader, StreamWriter, Directory, DirectoryInfo and Path in C#.
+
 ### <ins>File - Static members</ins>
 
-- Static members (simple, but performs a security check for each operation)
+- Static members (simple, but performs a security check for each operation).
+
+### <ins>Class FileInfo</ins>
+
+- Instance members (you need to instantiate a FileInfo class to use its methods).
